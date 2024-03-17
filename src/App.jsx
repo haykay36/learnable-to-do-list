9148a89898e4ccd2172ca22af4227fedc9937c59
haykay36 <akeemayinla@gmail.com>
2024-03-17 15:40:24 -0500

    to-do-list completed

--- src/App.jsx
+++ src/App.jsx
@@ -19,29 +19,40 @@ function App() {
     };
     setItems((oldList) => [...oldList, item]);
     setNewItem("");
-    console.log(items);
+  }
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
   }
 
   return (
-    <div className="App">
-      <h1>To-do List App</h1>
-      <input
-        type="text"
-        placeholder="Add an item..."
-        value={newItem}
-        onChange={() => setNewItem(e.target.value)}
-      />
-      <button onClick={() => addItem()}>Add</button>
-      <ul>
-        {items.map(item => {
-          return (
-            <li key={item.id}> {item.value}</li>
-          )
-        })}
-        <li>mon</li>
-        <li>tue</li>
-        <li>wed</li>
-      </ul>
+    <div className="App-cont">
+      <span className="App">
+        <h1>To-do List App</h1>
+        <input
+          type="text"
+          placeholder="Add an item..."
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button onClick={() => addItem()}>Add</button>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.id}>
+                {" "}
+                {item.value}{" "}
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  x
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </span>
     </div>
   );
 }
